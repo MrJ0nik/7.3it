c60b3dffadca58c309616b8562ff6d04e38b7d48
MrJ0nik <142501317+MrJ0nik@users.noreply.github.com>
2024-10-23 20:38:43 +0300

    first commit

---
+++ 7.3it/7.3it.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void CyclicShiftMatrix(int** matrix, int n, int k);
+int FindLastColumnWithMaxZeros(int** matrix, int n);
+void InputMatrix(int** matrix, int n);
+void PrintMatrix(int** matrix, int n);
+
+int main() {
+    int n, k;
+    cout << "Enter the size of the matrix (n x n): ";
+    cin >> n;
+    cout << "Enter the number of positions to shift (k): ";
+    cin >> k;
+
+    
+    int** matrix = new int* [n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[n];
+    }
+
+    
+    InputMatrix(matrix, n);
+
+   
+    cout << "Original matrix:" << endl;
+    PrintMatrix(matrix, n);
+
+   
+    CyclicShiftMatrix(matrix, n, k);
+
+    
+    cout << "Matrix after cyclic shift by " << k << " positions:" << endl;
+    PrintMatrix(matrix, n);
+
+   
+    int lastColWithMaxZeros = FindLastColumnWithMaxZeros(matrix, n);
+    cout << "The last column with the maximum number of zeros is: " << lastColWithMaxZeros << endl;
+
+    
+    for (int i = 0; i < n; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
+
+void InputMatrix(int** matrix, int n) {
+    cout << "Enter the matrix elements:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "matrix[" << i << "][" << j << "] = ";
+            cin >> matrix[i][j];
+        }
+    }
+}
+
+void PrintMatrix(int** matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << setw(4) << matrix[i][j];
+        }
+        cout << endl;
+    }
+}
+
+void CyclicShiftMatrix(int** matrix, int n, int k) {
+    for (int shift = 0; shift < k; shift++) {
+        int temp = matrix[0][0];
+        int i = 0, j = 0;
+
+        
+        for (i = 0; i < n - 1; i++) {
+            matrix[i][j] = matrix[i + 1][j];
+        }
+
+        
+        for (j = 0; j < n - 1; j++) {
+            matrix[i][j] = matrix[i][j + 1];
+        }
+
+        
+        for (i = n - 1; i > 0; i--) {
+            matrix[i][n - 1] = matrix[i - 1][n - 1];
+        }
+
+        
+        for (j = n - 1; j > 0; j--) {
+            matrix[0][j] = matrix[0][j - 1];
+        }
+
+        matrix[0][1] = temp;  
+    }
+}
+
+int FindLastColumnWithMaxZeros(int** matrix, int n) {
+    int maxZeroCount = 0;
+    int lastColWithMaxZeros = -1;
+
+    for (int j = 0; j < n; j++) {
+        int zeroCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][j] == 0) {
+                zeroCount++;
+            }
+        }
+        if (zeroCount >= maxZeroCount) {
+            maxZeroCount = zeroCount;
+            lastColWithMaxZeros = j;
+        }
+    }
+
+    return lastColWithMaxZeros;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,93 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../7.3it/7.3it.cpp"  
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(Test_CyclicShiftMatrix)
+		{
+			const int n = 3;
+			int** matrix = new int* [n];
+			for (int i = 0; i < n; i++) {
+				matrix[i] = new int[n];
+			}
+
+			
+			int data[3][3] = {
+				{ 1, 2, 3 },
+				{ 4, 5, 6 },
+				{ 7, 8, 9 }
+			};
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					matrix[i][j] = data[i][j];
+				}
+			}
+
+		
+			int expected[3][3] = {
+				{ 4, 1, 2 },
+				{ 7, 5, 3 },
+				{ 8, 9, 6 }
+			};
+
+			
+			CyclicShiftMatrix(matrix, n, 1);
+
+			
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					Assert::AreEqual(expected[i][j], matrix[i][j]);
+				}
+			}
+
+			
+			for (int i = 0; i < n; i++) {
+				delete[] matrix[i];
+			}
+			delete[] matrix;
+		}
+
+		TEST_METHOD(Test_FindLastColumnWithMaxZeros)
+		{
+			const int n = 3;
+			int** matrix = new int* [n];
+			for (int i = 0; i < n; i++) {
+				matrix[i] = new int[n];
+			}
+
+			
+			int data[3][3] = {
+				{ 1, 0, 0 },
+				{ 0, 1, 0 },
+				{ 0, 0, 1 }
+			};
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					matrix[i][j] = data[i][j];
+				}
+			}
+
+			
+			int expectedColumn = 2;
+
+			
+			int result = FindLastColumnWithMaxZeros(matrix, n);
+
+		
+			Assert::AreEqual(expectedColumn, result);
+
+			
+			for (int i = 0; i < n; i++) {
+				delete[] matrix[i];
+			}
+			delete[] matrix;
+		}
+	};
+}
